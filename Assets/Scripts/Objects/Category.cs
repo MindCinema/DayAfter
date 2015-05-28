778b49fd38d4d2fc8ac71d14bf4f3ccbc60b77aa
MichaelGER <michael.romer-web@web.de>
2015-05-28 19:10:47 +0200

    Basic inventory script, box prefab

---
+++ Assets/Scripts/Objects/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public abstract class Category : MonoBehaviour
+{
+    public abstract int Id
+    {
+        get;
+    }
+
+    public abstract string Name
+    {
+        get;
+        set;
+    }
+}
---
+++ Assets/Scripts/Objects/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private List<Item> Items;
+
+    public int MaxWeight
+    {
+        get;
+        set;
+    }
+
+    public double Weight
+    {
+        get
+        {
+            return Items.Sum(item => item.Weight);
+        }
+    }
+
+    private void Start()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void Update()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool AddItem(Item Item)
+    {
+        double itemWeight = Item.Weight;
+        if (MaxWeight - Weight < itemWeight)
+        {
+            return false;
+        }
+        Items.Add(Item);
+        return true;
+    }
+
+    public void RemoveItem(Item Item)
+    {
+        Items.Remove(Item);
+    }   
+}
