c89e351f75d7a8235724fdb99946dbd690b387de
MichaelGER <michael.romer-web@web.de>
2015-05-30 12:48:37 +0200

    UI tests, inventory script changes

--- Assets/Scripts/Objects/Inventory.cs
+++ Assets/Scripts/Objects/Inventory.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 
+[Serializable]
 public class Inventory : MonoBehaviour
 {
+    [SerializeField]
     private List<Item> Items;
+    private GameObject Canvas;
 
-    public int MaxWeight
-    {
-        get;
-        set;
-    }
+    public int MaxWeight;
 
     public double Weight
     {
@@ -25,31 +25,38 @@ public class Inventory : MonoBehaviour
     private void Start()
     {
         Items = new List<Item>();
+        Canvas = GameObject.Find("Canvas");
     }
 
     private void Update()
     {
+        if (Input.GetButton("Inventory"))
+        {
+            Debug.Log("Inventory");
+        }
+        var image = Canvas.GetComponentInChildren<Image>();
+        if (IsBoxNearPlayer())
+        {
+            Debug.Log("Bild zeigen");
+            image.CrossFadeAlpha(1, 0, true);
+        }
+        else
+        {
+            Debug.Log("Bild verstecken");
+            image.CrossFadeAlpha(0, 0, true);
+        }
     }
 
     private void OnMouseDown()
     {
-        Debug.Log("Klick!");
-        var player = GameObject.Find("Player");
-        if (player != null)
+        if (IsBoxNearPlayer())
         {
-            var playerPos = player.transform.position;
-            var boxPos = gameObject.transform.position;
-            var distance = Vector3.Distance(playerPos, boxPos);
-            if (distance < 5)
-            {
-                Debug.Log("Inventar auf");
-            }
+            Debug.Log("Inventar auf");
         }
     }
 
     private void OnMouseOver()
     {
-        Debug.Log("Mouse over");
     }
 
     public bool AddItem(Item Item)
@@ -66,5 +73,21 @@ public class Inventory : MonoBehaviour
     public void RemoveItem(Item Item)
     {
         Items.Remove(Item);
-    }   
+    }
+
+    private bool IsBoxNearPlayer()
+    {
+        var player = GameObject.Find("Player");
+        if (player != null)
+        {
+            var playerPos = player.transform.position;
+            var boxPos = gameObject.transform.position;
+            var distance = Vector3.Distance(playerPos, boxPos);
+            if (distance < 5)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
 }
